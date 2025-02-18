cd6cf04af8482a776f601d7cd077a9d636bd94a7
Shiro <bryanpeneyra@gmail.com>
2025-02-18 17:36:28 +0800

    1st question

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,20 @@
-
-
 public class Main {
     public static void main(String[] args) {
 
-            Person p = new Person("Bryan","512 rubyville",220);
-        System.out.println(p.name);
-        System.out.println("Test");
+        Question1();
+
+        }
+        public  static void Question1(){
+            Person p = new Person("Bryan","512 Rubyville", 22);
+            Person p1 = new Person("Alice","123 Main St.", 19);
+            Person p2 = new Person("Mark","712 Pinagbuhatan", 25);
+            Person p3 = new Person("Leah","22 New york Ave.", 32);
+            Person p4 = new Person("Ryan","66 Malinao St.", 29);
+
+            p.Objectinfo();
+            p1.Objectinfo();
+            p2.Objectinfo();
+            p3.Objectinfo();
+            p4.Objectinfo();
         }
     }
--- src/Person.java
+++ src/Person.java
@@ -7,5 +7,14 @@ public class Person {
         this.address = address;
         this.age = age;
 
+
+
+    }
+
+    public void Objectinfo() {
+        System.out.println("Name: " + name );
+        System.out.println("Age: "+ age);
+        System.out.println("Address: " + address);
+        System.out.println(" ");
     }
 }
