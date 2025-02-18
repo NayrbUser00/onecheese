3c99143ebf69eb71250a8a5ac51188afb57a2a7e
Shiro <bryanpeneyra@gmail.com>
2025-02-18 16:44:32 +0800

    Intial Test

---
+++ src/Person.java
@@ -0,0 +1,11 @@
+public class Person {
+        String name;
+        String address;
+        Integer age;
+    Person(String name, String address, Integer age){
+        this.name = name;
+        this.address = address;
+        this.age = age;
+
+    }
+}
